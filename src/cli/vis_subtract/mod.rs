bcd4dcc207937466c00cee35a6161e7598163a9a
Atom <tbjong2000@hotmail.com>
2025-05-21 20:16:06 +0800

    Function signature for beam_args.parse() has increased. Putting None at the end for these calls, I'm not sure if they affect the calculation or not! (Other than di-calibrate)

--- src/cli/vis_subtract/mod.rs
+++ src/cli/vis_subtract/mod.rs
@@ -187,6 +187,7 @@ impl VisSubtractArgs {
             obs_context.dipole_delays.clone(),
             obs_context.dipole_gains.clone(),
             Some(obs_context.input_data_type),
+            None,
         )?;
         let modelling_params @ ModellingParams {
             apply_precession, ..
