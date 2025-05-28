a94059ee6e105683fc7236f4e5fb236ee06b77c4
Atom <tbjong2000@hotmail.com>
2025-05-28 16:24:41 +0800

    ref freq didn't seem to be the problem

--- src/cli/di_calibrate/mod.rs
+++ src/cli/di_calibrate/mod.rs
@@ -279,7 +279,7 @@ impl DiCalArgs {
         let ska_beam_params = SkaBeamParams {
             phase_centre: obs_context.phase_centre,
             ska_site_latitude_rad: latitude_rad,
-            reference_frequency_hz: 170.24e6,
+            reference_frequency_hz: freq_centroid,
             number_of_stations: total_num_tiles,
         };
 
