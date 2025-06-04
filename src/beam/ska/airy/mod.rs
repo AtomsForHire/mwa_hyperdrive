5f9be814baae8f476868007377f2df29bf0b8bf2
Atom <tbjong2000@hotmail.com>
2025-06-04 12:57:42 +0800

    i'm tired

--- src/beam/ska/airy/mod.rs
+++ src/beam/ska/airy/mod.rs
@@ -59,7 +59,7 @@ impl SkaAiryBeam {
         tile_index: Option<usize>,
     ) -> Jones<f64> {
         // let index = tile_index.expect("Warning tile index is needed for Airy beam forming");
-        let index = tile_index.or(0);
+        let index = tile_index.unwrap_or(0 as usize);
 
         let station_angle = self.station_angle_rad[index];
         let feed_angle = self.feed_angle_rad[index];
