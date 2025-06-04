f9167f16b8e42a9b0e4a8cbc2338edf334dd3285
Atom <tbjong2000@hotmail.com>
2025-06-04 13:04:42 +0800

    more print!!!!!!

--- src/model/cpu.rs
+++ src/model/cpu.rs
@@ -134,6 +134,8 @@ impl<'a> SkyModellerCpu<'a> {
             tile_index_to_array_index_map.push(index);
         }
 
+        println!("NUM UNFLAGGED_TILES: {:?}", unflagged_tile_xyzs.len());
+        println!("NUM UNFLAGGED_TILES: {:?}", flagged_tiles.len());
         println!("UNIQUE_TILES: {:?}", unique_tiles);
 
         let mut unique_beam_freqs = vec![];
