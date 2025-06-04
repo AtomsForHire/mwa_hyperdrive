5af00aead9b04e262371c8dbc074267e8236f945
Atom <tbjong2000@hotmail.com>
2025-06-04 13:07:40 +0800

    nrows()

--- src/model/cpu.rs
+++ src/model/cpu.rs
@@ -134,8 +134,8 @@ impl<'a> SkyModellerCpu<'a> {
             tile_index_to_array_index_map.push(index);
         }
 
-        println!("NUM GAIN ROWS: {:?}", gains.rows());
-        println!("NUM DELAYS ROWS: {:?}", delays.rows());
+        println!("NUM GAIN ROWS: {:?}", gains.nrows());
+        println!("NUM DELAYS ROWS: {:?}", delays.nrows());
         println!("NUM UNFLAGGED_TILES: {:?}", unflagged_tile_xyzs.len());
         println!("NUM UNFLAGGED_TILES: {:?}", flagged_tiles.len());
         println!("UNIQUE_TILES: {:?}", unique_tiles);
