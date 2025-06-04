2553ff1d05fedd3b5ad85090a2800b4c93aa4707
Atom <tbjong2000@hotmail.com>
2025-06-04 13:25:44 +0800

    Should have fixed problem in CPU code path
    The problem was that the length of unique_tiles was 1 because we don't
    have gains or delays, so then code creates identical gains and delays
    for all tiles. Therefore only 1 tile is unique

--- src/model/cpu.rs
+++ src/model/cpu.rs
@@ -104,41 +104,50 @@ impl<'a> SkyModellerCpu<'a> {
         let mut unique_tiles = vec![];
         let mut tile_index_to_array_index_map = Vec::with_capacity(total_num_tiles);
 
-        let mut i_array_tile = 0;
-        for (i_tile, (gains, delays)) in gains.outer_iter().zip(delays.outer_iter()).enumerate() {
-            if flagged_tiles.contains(&i_tile) {
-                tile_index_to_array_index_map.push(0);
-                continue;
+        let beam_type = beam.get_beam_type();
+        if matches!(beam_type, BeamType::SkaAiry | BeamType::SkaGaussian) {
+            let mut current_unique_idx_counter = 0;
+            for i_tile in 0..total_num_tiles {
+                if flagged_tiles.contains(&i_tile) {
+                    tile_index_to_array_index_map.push(0);
+                } else {
+                    unique_tiles.push(i_tile);
+                    tile_index_to_array_index_map.push(current_unique_idx_counter);
+                    current_unique_idx_counter += 1;
+                }
             }
-
-            let (gains, delays) = fix_amps_ndarray(gains, delays);
-
-            let mut unique_tile_hasher = DefaultHasher::new();
-            delays.hash(&mut unique_tile_hasher);
-            // We can't hash f64 values, but we can hash their bits.
-            for gain in gains {
-                gain.to_bits().hash(&mut unique_tile_hasher);
-            }
-            let unique_tile_hash = unique_tile_hasher.finish();
-            let index = if let Some((_, index)) = unique_hashes
-                .iter()
-                .find(|(unique_hash, _)| *unique_hash == unique_tile_hash)
+        } else {
+            let mut i_array_tile = 0;
+            for (i_tile, (gains, delays)) in gains.outer_iter().zip(delays.outer_iter()).enumerate()
             {
-                *index
-            } else {
-                unique_hashes.push((unique_tile_hash, i_array_tile));
-                unique_tiles.push(i_tile);
-                i_array_tile += 1;
-                i_array_tile - 1
-            };
-            tile_index_to_array_index_map.push(index);
-        }
+                if flagged_tiles.contains(&i_tile) {
+                    tile_index_to_array_index_map.push(0);
+                    continue;
+                }
 
-        println!("NUM GAIN ROWS: {:?}", gains.nrows());
-        println!("NUM DELAYS ROWS: {:?}", delays.nrows());
-        println!("NUM UNFLAGGED_TILES: {:?}", unflagged_tile_xyzs.len());
-        println!("NUM UNFLAGGED_TILES: {:?}", flagged_tiles.len());
-        println!("UNIQUE_TILES: {:?}", unique_tiles);
+                let (gains, delays) = fix_amps_ndarray(gains, delays);
+
+                let mut unique_tile_hasher = DefaultHasher::new();
+                delays.hash(&mut unique_tile_hasher);
+                // We can't hash f64 values, but we can hash their bits.
+                for gain in gains {
+                    gain.to_bits().hash(&mut unique_tile_hasher);
+                }
+                let unique_tile_hash = unique_tile_hasher.finish();
+                let index = if let Some((_, index)) = unique_hashes
+                    .iter()
+                    .find(|(unique_hash, _)| *unique_hash == unique_tile_hash)
+                {
+                    *index
+                } else {
+                    unique_hashes.push((unique_tile_hash, i_array_tile));
+                    unique_tiles.push(i_tile);
+                    i_array_tile += 1;
+                    i_array_tile - 1
+                };
+                tile_index_to_array_index_map.push(index);
+            }
+        }
 
         let mut unique_beam_freqs = vec![];
         let mut unique_freqs = vec![];
