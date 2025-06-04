f2de7383df563833d482960cec15f612c4d15522
Atom <tbjong2000@hotmail.com>
2025-06-04 12:31:32 +0800

    Swapped out i_unique_tile for i_tile when getting beam responses

--- src/model/cpu.rs
+++ src/model/cpu.rs
@@ -207,7 +207,7 @@ impl<'a> SkyModellerCpu<'a> {
                 self.beam.calc_jones_array_inner(
                     azels,
                     *freq,
-                    Some(i_unique_tile),
+                    Some(i_tile),
                     array_latitude_rad,
                     slice,
                 )?;
