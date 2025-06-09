254c2c00c026978878ba8a7bc10156b7c609cbd0
Atom <tbjong2000@hotmail.com>
2025-06-09 15:38:04 +0800

    For some reason get_cell() is detecting the element_offset column to be f64, when it should be Array2<f64>. Try the more explicit syntax

--- src/io/read/ms/mod.rs
+++ src/io/read/ms/mod.rs
@@ -930,7 +930,7 @@ impl MsReader {
         let num_rows = phased_array_table.n_rows();
         let mut feed_coordinates: Vec<ndarray::Array2<f64>> = vec![];
         for row_idx in 0..num_rows {
-            match phased_array_table.get_cell("ELEMENT_OFFSET", row_idx) {
+            match phased_array_table.get_cell::<ndarray::Array2<f64>>("ELEMENT_OFFSET", row_idx) {
                 Ok(offsets) => {
                     feed_coordinates.push(offsets);
                 }
