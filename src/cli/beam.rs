9e1ab27cc8b8650417024a9a051284da2b24e747
Atom <tbjong2000@hotmail.com>
2025-07-02 12:08:36 +0800

    Write out all elements of Jones matrix

--- src/cli/beam.rs
+++ src/cli/beam.rs
@@ -120,8 +120,10 @@ fn calc_cpu(args: &BeamArgs) -> Result<(), HyperdriveError> {
     )?;
     // Write out total + separate x, y components
     let mut out = BufWriter::new(File::create(output)?);
-    let mut out_x = BufWriter::new(File::create("beam_responses_x.tsv")?);
-    let mut out_y = BufWriter::new(File::create("beam_responses_y.tsv")?);
+    let mut out_00 = BufWriter::new(File::create("beam_responses_00.tsv")?);
+    let mut out_01 = BufWriter::new(File::create("beam_responses_01.tsv")?);
+    let mut out_10 = BufWriter::new(File::create("beam_responses_10.tsv")?);
+    let mut out_11 = BufWriter::new(File::create("beam_responses_11.tsv")?);
 
     let azels: Vec<_> = gen_azzas(max_za.to_radians(), step.to_radians())
         .map(|(az, za)| AzEl::from_radians(az, FRAC_PI_2 - za))
@@ -139,9 +141,10 @@ fn calc_cpu(args: &BeamArgs) -> Result<(), HyperdriveError> {
             j[0].norm() + j[3].norm()
         )?;
 
-        writeln!(&mut out_x, "{}\t{}\t{:e}", azel.az, azel.za(), j[0].norm())?;
-
-        writeln!(&mut out_y, "{}\t{}\t{:e}", azel.az, azel.za(), j[3].norm())?;
+        writeln!(&mut out_00, "{}\t{}\t{:e}", azel.az, azel.za(), j[0].norm())?;
+        writeln!(&mut out_01, "{}\t{}\t{:e}", azel.az, azel.za(), j[1].norm())?;
+        writeln!(&mut out_10, "{}\t{}\t{:e}", azel.az, azel.za(), j[2].norm())?;
+        writeln!(&mut out_11, "{}\t{}\t{:e}", azel.az, azel.za(), j[3].norm())?;
     }
 
     Ok(())
