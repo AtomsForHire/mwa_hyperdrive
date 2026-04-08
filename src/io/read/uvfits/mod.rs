9659389cbdc16d752059e3f9861d4ced9dace5f5
Atom <tbjong2000@hotmail.com>
2026-04-08 19:58:22 +0800

    obs_context now has two additional arguments that are only obtainable from OSKAR measurement sets. Pass None when reading from uvfits or raw

--- src/io/read/uvfits/mod.rs
+++ src/io/read/uvfits/mod.rs
@@ -675,6 +675,8 @@ impl UvfitsReader {
             flagged_fine_chans: vec![],
             flagged_fine_chans_per_coarse_chan: None,
             polarisations: metadata.pols,
+            feed_angles: None,
+            feed_coordindates: None,
         };
 
         Ok(UvfitsReader {
